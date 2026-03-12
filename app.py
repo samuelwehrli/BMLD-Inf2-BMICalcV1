38f276a7b49b42dcef303454ee81b29a69b0adee
Samuel Wehrli <samuel.wehrli@gmail.com>
2026-03-12 08:21:14 +0100

    update names

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/bmi_calculator.py", title="BMI-Rechner", icon=":material/calculate:")
+pg_calculator = st.Page("views/calculator.py", title="BMI-Rechner", icon=":material/calculate:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_calculator])
 pg.run()
